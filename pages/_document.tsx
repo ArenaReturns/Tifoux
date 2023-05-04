26ffea13b8c26841ceb22088fbf1c2a23db3fabe
Jordan Rey <rey.jordan.1998@gmail.com>
2023-05-04 10:47:04 +0200

    html document lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentProps } from "next/document";
+import i18nextConfig from "../next-i18next.config";
 
-export default function Document() {
-  return (
-    <Html lang="fr">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document<DocumentProps> {
+  render() {
+    const currentLocale = this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
+
+    return (
+      <Html lang={currentLocale}>
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
