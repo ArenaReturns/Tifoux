cd04306dbac9a4a686de4a057539813d878b8381
Jordan Rey <rey.jordan.1998@gmail.com>
2023-05-04 12:33:03 +0200

    display version in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Trans, useTranslation } from "next-i18next";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
 
 export const Footer = () => {
   const { t } = useTranslation("common");
@@ -7,7 +10,9 @@ export const Footer = () => {
   return (
     <footer>
       <div className="mx-auto overflow-hidden py-8 px-6 sm:py-10 lg:px-8">
-        <p className="mt-8 text-center text-xs leading-5 text-gray-100 text-opacity-50">
+        <p className="text-center text-xs leading-5 text-gray-100 text-opacity-50">
+          Tifoux v{publicRuntimeConfig?.version}
+          <br />
           <Trans t={t} i18nKey="footer" />
         </p>
       </div>
--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 // @ts-check
 const { i18n } = require("./next-i18next.config");
+const { version } = require("./package.json");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  publicRuntimeConfig: {
+    version,
+  },
 };
 
 module.exports = nextConfig;
