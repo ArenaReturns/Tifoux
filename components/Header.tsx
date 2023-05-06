7136dd0f64939b374892532cc6050a2d04530876
Jydett <48091696+Jydett@users.noreply.github.com>
2023-05-06 22:10:00 +0200

    Feat/new navbar (#9)
    * testing new navbar
    
    * fixed new nav style
    
    * chore(css): no shrinkable logo
     - discord link added
     - faq link added
    
    * feat: 1st iteration of FAQ
    
    ---------
    
    Co-authored-by: Jordan Rey <rey.jordan.1998@gmail.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,6 +12,7 @@ interface HeaderProps {}
 const navigation = [
   { name: "navbar.links.index", href: "/", isExternal: false },
   { name: "navbar.links.download", href: "/download", isExternal: false },
+  { name: "navbar.links.faq", href: "/faq", isExternal: false },
   {
     name: "navbar.links.discord",
     href: "https://discord.gg/arenareturns",
@@ -22,7 +23,15 @@ const navigation = [
     href: "https://twitter.com/ArenaReturns",
     isExternal: true,
   },
+  {
+    name: "navbar.links.github",
+    href: "https://github.com/ArenaReturns",
+    isExternal: true,
+  },
 ];
+const middleNavIndex = Math.ceil(navigation.length / 2);
+const navigationLeft = navigation.splice(0, middleNavIndex);
+const navigationRight = navigation.splice(-middleNavIndex);
 
 const lang = [
   { name: "Français", locale: "fr", flag: "fr" },
@@ -47,15 +56,12 @@ export const Header: React.FC<HeaderProps> = ({}) => {
 
   return (
     <header className="bg-[#372820]">
-      <nav className="mx-auto flex max-w-5xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1 z-10">
-          <Link href="/" className="lg:-my-8 lg:translate-y-6 hover:scale-110 hover:-rotate-3 duration-500 transition-all">
+      <nav className="mt-3 mx-auto flex max-w-5xl items-center justify-center p-4 lg:p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:hidden items-center justify-between w-full">
+          <Link href="/">
             <span className="sr-only">Arena Returns</span>
-            <Image className="hidden lg:block h-8 lg:h-28 w-auto" src="/logo.png" alt="" width={202} height={128} />
-            <Image className="lg:hidden h-10 w-auto" src="/icon.png" alt="" width={202} height={128} />
+            <Image className="h-12 w-auto" src="/logo-square.png" alt="" width={202} height={128} />
           </Link>
-        </div>
-        <div className="flex lg:hidden">
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-100"
@@ -65,21 +71,42 @@ export const Header: React.FC<HeaderProps> = ({}) => {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-16">
-          {navigation.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className={`${rocknRollFont.className} text-sm font-normal leading-6 text-yellow-50 hover:text-yellow-500 duration-500 transition-colors`}
-              target={item.isExternal ? "_blank" : "_self"}
-              rel={item.isExternal ? "noopener noreferrer" : undefined}
-            >
-              {t(item.name)}
-            </Link>
+        <div className="hidden lg:flex lg:gap-x-20 lg:justify-around lg:flex-1">
+          {navigationLeft.map((item) => (
+              <React.Fragment key={item.name}>
+              <Link
+                href={item.href}
+                className={`${rocknRollFont.className} text-sm font-normal leading-6 text-yellow-50 hover:text-yellow-500 duration-500 transition-colors`}
+                target={item.isExternal ? "_blank" : "_self"}
+                rel={item.isExternal ? "noopener noreferrer" : undefined}
+              >
+                {t(item.name)}
+              </Link>
+            </React.Fragment>
           ))}
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Popover className="relative">
+        <div className="hidden lg:flex lg:z-10 shrink-0">
+          <Link href="/" className="lg:-my-16 lg:translate-y-6">
+            <span className="sr-only">Arena Returns</span>
+            <Image className="h-8 lg:h-32 w-auto" src="/logo.png" alt="" width={202} height={128} />
+          </Link>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-20 justify-around flex-1">
+          {navigationRight.map((item) => (
+            <React.Fragment key={item.name}>
+                <Link
+                    href={item.href}
+                    className={`${rocknRollFont.className} text-sm font-normal leading-6 text-yellow-50 hover:text-yellow-500 duration-500 transition-colors`}
+                    target={item.isExternal ? "_blank" : "_self"}
+                    rel={item.isExternal ? "noopener noreferrer" : undefined}
+                >
+                  {t(item.name)}
+                </Link>
+              </React.Fragment>
+          ))}
+        </div>
+        <div className="lg:block hidden absolute top-3 right-0 z-50">
+          <Popover className="fixed right-3">
             <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-yellow-50">
               {lang.map((item) => (
                 <span key={item.name} className={`fi fi-${item.flag}`} />
@@ -96,7 +123,7 @@ export const Header: React.FC<HeaderProps> = ({}) => {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-56 rounded-xl bg-[#755938] p-2 shadow-lg ring-1 ring-amber-900/20">
+              <Popover.Panel className="absolute -left-12 top-full z-10 mt-3 w-56 rounded-xl bg-[#755938] p-2 shadow-lg ring-1 ring-amber-900/20">
                 {lang.map((item) => (
                   <Popover.Button
                     key={item.name}
@@ -114,11 +141,11 @@ export const Header: React.FC<HeaderProps> = ({}) => {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#372820] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#372820] px-4 py-4 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.5">
+            <Link href="/">
               <span className="sr-only">Arena Returns</span>
-              <Image className="h-10 w-auto" src="/icon.png" alt="" width={202} height={128} />
+              <Image className="h-12 w-auto" src="/logo-square.png" alt="" width={202} height={128} />
             </Link>
             <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-100" onClick={() => setMobileMenuOpen(false)}>
               <span className="sr-only">{t("navbar.mobile.close")}</span>
@@ -128,7 +155,7 @@ export const Header: React.FC<HeaderProps> = ({}) => {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                {navigation.map((item) => (
+                {navigationLeft.concat(navigationRight).map((item) => (
                   <Link
                     key={item.name}
                     href={item.href}
---
+++ pages/faq.tsx
@@ -0,0 +1,62 @@
+import {Trans, useTranslation} from "next-i18next";
+import { Disclosure } from '@headlessui/react'
+import Head from "next/head";
+import {Header} from "@/components/Header";
+import { interFont } from "@/utils/fonts";
+import {Footer} from "../components/Footer";
+import {Hero} from "../components/Hero";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import { ChevronUpIcon } from '@heroicons/react/20/solid'
+
+export default function FAQ() {
+    const { t } = useTranslation("faq");
+    const faq = Object.keys(t('faq', { returnObjects: true }));
+
+    return (
+        <>
+            <Head>
+                <title>{`Arena Returns - ${t("html.title")}`}</title>
+                <meta name="description" content={`Arena Returns - ${t("html.description")}`} />
+            </Head>
+            <Header />
+            <main className={`${interFont.className} container mx-auto max-w-5xl`}>
+                <Hero title={t("hero.title")} />
+                <div className="bg-[#755938] rounded-none xl:rounded-2xl mt-0 xl:mt-4 overflow-hidden">
+                    <div className="h-full items-center justify-between px-6 lg:px-8 py-12 bg-gradient-to-r from-[#664C32] to-[#755938]">
+                        {faq.length == 0 && <p>{t('noQuestion')}</p>}
+                        {faq.map((q, i) => {
+                            return (
+                                <Disclosure key={i} as="div" className={`${i > 0 ? 'mt-5' : ''} w-full`}>
+                                    {({ open }) => (
+                                        <>
+                                            <Disclosure.Button className="mb-2 w-full rounded-lg bg-amber-100 px-4 py-2 text-left text-sm font-medium text-amber-900 hover:bg-amber-200 focus:outline-none focus-visible:ring focus-visible:ring-amber-500 focus-visible:ring-opacity-75">
+                                                <span><Trans t={t} i18nKey={'faq.' + q + '.q'} /></span>
+                                                <ChevronUpIcon
+                                                    className={`${
+                                                        open ? 'rotate-180 transform' : ''
+                                                    } h-5 w-5 text-amber-500 float-right`}
+                                                />
+                                            </Disclosure.Button>
+                                            <Disclosure.Panel>
+                                                <Trans t={t} i18nKey={'faq.' + q + '.a'} />
+                                            </Disclosure.Panel>
+                                        </>
+                                    )}
+                                </Disclosure>
+                            )
+                        })}
+                    </div>
+                </div>
+                <Footer />
+            </main>
+        </>
+    );
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common", "faq"])),
+        },
+    };
+}
