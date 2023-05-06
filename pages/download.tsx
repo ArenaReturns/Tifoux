c4c3b56edcd1a4312be47072b8520807a72e7be0
Jordan Rey <rey.jordan.1998@gmail.com>
2023-05-06 22:01:58 +0200

    feat: dynamic download (#8)
    * feat: dynamic download
    
    * better integration (thx Jydett)

--- pages/download.tsx
+++ pages/download.tsx
@@ -7,9 +7,14 @@ import { Header } from "@/components/Header";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Trans, useTranslation } from "next-i18next";
 import { copperplateFont, interFont, rocknRollFont } from "@/utils/fonts";
+import dynamic from "next/dynamic";
 
 export default function Download() {
   const { t } = useTranslation("download");
+
+  const DynamicDownload = dynamic(() => import("../components/DynamicDownload"), {
+    ssr: false,
+  });
 
   return (
     <>
@@ -27,25 +32,7 @@ export default function Download() {
               <p className="mt-6">
                 <Trans t={t} i18nKey="download.description" />
               </p>
-              <div className="mt-8 space-x-2 md:space-x-4 flex justify-center w-full">
-                <Link
-                  className={`${rocknRollFont.className} max-w-full items-center inline-flex border-2 border-[#372820] px-6 py-4 rounded-lg bg-gradient-to-t from-amber-800 via-amber-600 to-amber-600 hover:from-amber-800 hover:via-amber-600 hover:to-amber-500`}
-                  href="#"
-                  rel="noopener noreferrer"
-                >
-                  <svg className="text-white h-6 w-6 mr-2" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <title>Windows</title>
-                    <path d="M0 3.449L9.75 2.1v9.451H0m10.949-9.602L24 0v11.4H10.949M0 12.6h9.75v9.451L0 20.699M10.949 12.6H24V24l-12.9-1.801" />
-                  </svg>
-                  <span>{t("download.windows")}</span>
-                </Link>
-              </div>
-              <div className="flex flex-row gap-x-6 mt-4 justify-center w-full">
-                <p className="text-stone-500 text-sm">{t("download.macos")}</p>
-                <Link href="#" className="text-stone-400 underline text-sm hover:text-stone-200">
-                  {t("download.linux")}
-                </Link>
-              </div>
+              <DynamicDownload />
             </div>
             <Image
               className="mt-6 md:mt-0 rounded-lg border border-[#BEA979]"
